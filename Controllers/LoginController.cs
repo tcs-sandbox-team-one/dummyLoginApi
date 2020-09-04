491c3eea758ec1c0e2c55f447d8b6394ddffdd92
tsubhojit <subhojit.tarafdartcs.com>
2020-09-04 14:56:10 +0530

    checking in the project

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dummyLoginApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/Login
+        [HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Login/5
+        [HttpGet("{id}/{pwd}", Name = "Get")]
+        public HttpResponseMessage Get(string id, string pwd)
+        {
+            if ((!string.IsNullOrEmpty(id)) && (!string.IsNullOrEmpty(pwd)))
+            {
+                if ((id == "admin") && (pwd=="pwd"))
+                {
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+        }
+
+        //// POST: api/Login
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Login/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
